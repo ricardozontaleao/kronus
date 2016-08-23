cbd1c561770942bf14970079f08a2f1449b66461
Rafael <rafael.rafaelsilva@gmail.com>
2016-08-22 22:22:24 -0300

    Correção do datasource

--- src/main/java/br/com/empresa/app/kronus/conf/JPAConfiguration.java
+++ src/main/java/br/com/empresa/app/kronus/conf/JPAConfiguration.java
@@ -7,7 +7,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -20,45 +20,41 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JPAConfiguration {
 
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean( DataSource dataSource ) {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-		
+
 		em.setDataSource(dataSource);
-		//em.setPersistenceUnitName("PostgreSQLDS");
-		em.setPackagesToScan( new String[]{"br.com.empresa.app.kronus.models"});
-		
+		// em.setPersistenceUnitName("PostgreSQLDS");
+		em.setPackagesToScan(new String[] { "br.com.empresa.app.kronus.models" });
+
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		em.setJpaVendorAdapter(vendorAdapter);
 		em.setJpaProperties(additionalProperties());
-		
+
 		return em;
 	}
-	
+
 	@Bean
 	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/kronus");
-		dataSource.setUsername("kronus");
-		dataSource.setPassword("kronus");
-		
+		JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+		dsLookup.setResourceRef(true);
+		DataSource dataSource = dsLookup.getDataSource("java:jboss/datasources/PostgreSQLDS");
 		return dataSource;
 	}
-	
+
 	private Properties additionalProperties() {
 		Properties properties = new Properties();
-		
+
 		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.format_sql", "true");
 		return properties;
 	}
-	
+
 	@Bean
-	public PlatformTransactionManager transactionManager( EntityManagerFactory managerFactory ) {
+	public PlatformTransactionManager transactionManager(EntityManagerFactory managerFactory) {
 		JpaTransactionManager transactionManager = new JpaTransactionManager();
 		transactionManager.setEntityManagerFactory(managerFactory);
-		 return transactionManager;
+		return transactionManager;
 	}
 }
